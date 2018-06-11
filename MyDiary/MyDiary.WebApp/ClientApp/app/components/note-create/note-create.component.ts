2eb5ad44c5722ffad46089a610c08075a59772d8
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2018-06-11 12:16:21 +0300

    Added file upload functionality to note-create.component

--- MyDiary/MyDiary.WebApp/ClientApp/app/components/note-create/note-create.component.ts
+++ MyDiary/MyDiary.WebApp/ClientApp/app/components/note-create/note-create.component.ts
@@ -11,17 +11,34 @@ import { NgForm, FormGroup, FormBuilder, FormControl, Validators } from '@angula
 export class NoteCreateComponent implements OnInit {
     createComponentForm: FormGroup;
     description: string = '';
+    fileToUpload: any;
 
     constructor(private notesService: NotesService, private formbuilder: FormBuilder) {
     }
 
     ngOnInit() {
         this.createComponentForm = this.formbuilder.group({
-            description: ['', [Validators.required]]
+            description: ['', [Validators.required]],
+            file: null
         });
     }
 
     onSubmit(createComponentForm: NgForm) {
         console.log(createComponentForm);
+    }
+
+    onFileChange(event: any) {
+        let reader = new FileReader();
+        if (event.target.files && event.target.files.length > 0) {
+            let file = event.target.files[0];
+            reader.readAsDataURL(file);
+            reader.onload = () => {
+                this.createComponentForm.controls['file'].setValue({
+                    filename: file.name,
+                    filetype: file.type,
+                    value: reader.result.split(',')[1]
+                })                            
+            };
+        }
     }
 }
