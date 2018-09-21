3f33b05cdca0f13fef8fe2aac7ad431bd791bf0a
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2018-09-21 12:43:14 +0300

    Added Note model

---
+++ MD.MongoDB/MD.MongoDB.DAL/Note.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MD.MongoDB.DAL
+{
+    public class Note
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Time of note creation.
+        /// </summary>
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Text of the note.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// List of all files that are linked to note.
+        /// </summary>
+        public List<ObjectId> FilesIds { get; set; }
+    }
+}
