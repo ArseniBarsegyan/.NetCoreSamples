87bbdbe55e4e11f8ca67ce02f314c024b9f508a2
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2018-09-21 13:13:30 +0300

    Added Console client, NoteRepository with GetAll() method

---
+++ MD.MongoDB/MD.MongoDB.ConsoleClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MD.MongoDB.DAL;
+using MongoDB.Driver;
+
+namespace MD.MongoDB.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "mongodb://localhost:27017";
+            Task<List<Note>> task = Task.Run(async () => await GetAllNotes(connectionString));
+            task.Wait();
+            var result = task.Result;
+        }
+
+        private static async Task<List<Note>> GetAllNotes(string connectionString)
+        {
+            var repository = new NoteRepository(connectionString);
+            return await repository.GetAll();
+        }
+    }
+}
---
+++ MD.MongoDB/MD.MongoDB.DAL/NoteRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MD.MongoDB.DAL
+{
+    public class NoteRepository
+    {
+        private readonly IMongoCollection<Note> _notes;
+
+        public NoteRepository(string connectionString)
+        {
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("MDMongoDB");
+            _notes = database.GetCollection<Note>("notes");
+        }
+
+        public async Task<List<Note>> GetAll()
+        {
+            var filter = new BsonDocument();
+            return await _notes.Find(filter).ToListAsync();
+        }
+    }
+}
