fe2390af336e039349f749d7e4e5280c483b38cb
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2018-09-21 16:11:51 +0300

    Added notes-list component

---
+++ MD.MongoDB/MD.MongoDB.WebApp/ClientApp/src/app/notes-list/notes-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-notes-list',
+  templateUrl: './notes-list.component.html',
+  styleUrls: ['./notes-list.component.css']
+})
+export class NotesListComponent implements OnInit {
+  public notes: Note[];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<Note[]>(baseUrl + 'api/Notes/AllNotes')
+      .subscribe(result => {
+        this.notes = result;
+        console.log(result);
+      }, error => console.error(error));
+  }
+
+  ngOnInit() {
+  }
+}
+
+interface Note {
+  id: string;
+  date: Date;
+  description: string;
+  filesIds: string[];
+}
---
+++ MD.MongoDB/MD.MongoDB.WebApp/Controllers/NotesController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MD.MongoDB.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MD.MongoDB.WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class NotesController : Controller
+    {
+        private readonly NoteRepository _repository;
+
+        public NotesController()
+        {
+            _repository = new NoteRepository("mongodb://localhost:27017");
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IEnumerable<Note>> AllNotes()
+        {
+            return await _repository.GetAllAsync();
+        }
+    }
+}
