228844e3c68ad73e256ebad3ce4fd8763e77fa16
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2018-09-24 17:42:25 +0300

    create note successful

--- MD.MongoDB/MD.MongoDB.WebApp/ClientApp/src/app/app.module.ts
+++ MD.MongoDB/MD.MongoDB.WebApp/ClientApp/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+import {ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
@@ -28,7 +28,7 @@ import { CreateNoteComponent } from './create-note/create-note.component';
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
-    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
--- MD.MongoDB/MD.MongoDB.WebApp/ClientApp/src/app/create-note/create-note.component.ts
+++ MD.MongoDB/MD.MongoDB.WebApp/ClientApp/src/app/create-note/create-note.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NotesService } from '../notes.service';
 
 @Component({
   selector: 'app-create-note',
@@ -6,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-note.component.css']
 })
 export class CreateNoteComponent implements OnInit {
+  createComponentForm: FormGroup;
+  description = '';
 
-  constructor() { }
+  constructor(private notesService: NotesService, private formbuilder: FormBuilder, private router: Router) {
+  }
 
   ngOnInit() {
+    this.createComponentForm = this.formbuilder.group({
+      description: ['', [Validators.required]],
+      file: null
+    });
+  }
+
+  onFileChange(event: any, fileNames: HTMLLabelElement) {
+    const reader = new FileReader();
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+
+      let files = event.target.files;
+
+      for (let el of files) {
+        fileNames.innerHTML += el.name + ' ';
+      }
+
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.createComponentForm.controls['file'].setValue({
+          filename: file.name,
+          filetype: file.type,
+          value: reader.result.split(',')[1]
+        });
+      };
+    }
+  }
+
+  onSubmit(createComponentForm: NgForm) {
+    const note: Note = {
+      id: '',
+      date: new Date(),
+      description: createComponentForm.controls['description'].value,
+      filesIds: []
+    };
+
+    this.notesService.createNote(note);
+    this.router.navigate(['notes-list']);
   }
 }
